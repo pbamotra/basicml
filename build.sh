d87efd89071fe396b4ca3a6cbfa586106bc7a4b6
Pankesh Bamotra <pankesh.bamotra@gmail.com>
2021-06-13 11:27:13 -0700

    [BLOG-0] Fix for double prefix in second brain notes

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 echo '1/4 Installing yarn dependencies'
 yarn add gatsby react react-dom
-yarn add https://github.com/pbamotra/gatsby-theme-andy#pbamotra
+yarn add https://github.com/pbamotra/gatsby-theme-andy#pbamotrav2
 
 echo '2/4 Yarn build'
 yarn clean && yarn build
