5a621edb95b1fdfe15aac826cb14d5dd546243e6
Pankesh Bamotra <pankesh.bamotra@gmail.com>
2021-06-26 00:15:24 -0700

    [BLOG-0] Changed notes path from second-brain to notes

--- postbuild.sh
+++ postbuild.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo '1/2 Rename build directory to "second-brain"'
-rm -rf second-brain
-mv public second-brain
+echo '1/2 Rename build directory to "notes"'
+rm -rf notes
+mv public notes
 
 echo '2/2 Finishing'
