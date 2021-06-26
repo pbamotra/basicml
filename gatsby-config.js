6c0881b874af8e438cf306a6894bdc2a3776c8fc
Pankesh Bamotra <pankesh.bamotra@gmail.com>
2021-06-26 02:50:32 -0700

    [BLOG-0] Adds a redundant subtitle to notes page

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,8 @@
 module.exports = {
   pathPrefix: `notes`,
   siteMetadata: {
-    title: 'Pankesh Bamotra'
+    title: 'Pankesh Bamotra',
+    subtitle: 'Notes Home'
   },
   plugins: [`gatsby-theme-andy`],
 };
